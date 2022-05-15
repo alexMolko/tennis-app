412e90970adc1ea0b3f120579bf9dd3148b35654
alexMolko <carlosmayarodriguez@gmail.com>
2022-05-15 16:49:25 -0500

    Adds login form and page

--- app/components/Forms/LoginForm.js
+++ app/components/Forms/LoginForm.js
@@ -13,18 +13,13 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import AllInclusive from '@material-ui/icons/AllInclusive';
-import Brightness5 from '@material-ui/icons/Brightness5';
-import People from '@material-ui/icons/People';
 import ArrowForward from '@material-ui/icons/ArrowForward';
 import Paper from '@material-ui/core/Paper';
-import Icon from '@material-ui/core/Icon';
 import Hidden from '@material-ui/core/Hidden';
 import brand from 'dan-api/dummy/brand';
 import logo from 'dan-images/logo.svg';
 import { TextFieldRedux, CheckboxRedux } from './ReduxFormMUI';
 import styles from './user-jss';
-import { ContentDivider } from '../Divider';
 
 // validation functions
 const required = value => (value === null ? 'Required' : undefined);
@@ -71,35 +66,11 @@ function LoginForm(props) {
               <img src={logo} alt={brand.name} />
               {brand.name}
             </NavLink>
-            <Button size="small" className={classes.buttonLink} component={LinkBtn} to="/register">
-              <Icon className={classes.icon}>arrow_forward</Icon>
-              Create new account
-            </Button>
           </div>
         </Hidden>
         <Typography variant="h4" className={classes.title} gutterBottom>
-          Sign In
+          Iniciar sesión
         </Typography>
-        <Typography variant="caption" className={classes.subtitle} gutterBottom align="center">
-          Lorem ipsum dolor sit amet
-        </Typography>
-        <section className={classes.socmedLogin}>
-          <div className={classes.btnArea}>
-            <Button variant="outlined" size="small" className={classes.redBtn} type="button">
-              <AllInclusive className={classNames(classes.leftIcon, classes.iconSmall)} />
-              Socmed 1
-            </Button>
-            <Button variant="outlined" size="small" className={classes.blueBtn} type="button">
-              <Brightness5 className={classNames(classes.leftIcon, classes.iconSmall)} />
-              Socmed 2
-            </Button>
-            <Button variant="outlined" size="small" className={classes.cyanBtn} type="button">
-              <People className={classNames(classes.leftIcon, classes.iconSmall)} />
-              Socmed 3
-            </Button>
-          </div>
-          <ContentDivider content="Or sign in with email" />
-        </section>
         <section className={classes.formWrap}>
           <form onSubmit={handleSubmit}>
             <div>
@@ -107,8 +78,8 @@ function LoginForm(props) {
                 <Field
                   name="email"
                   component={TextFieldRedux}
-                  placeholder="Your Email"
-                  label="Your Email"
+                  placeholder="Su email"
+                  label="Su email"
                   required
                   validate={[required, email]}
                   className={classes.field}
@@ -121,7 +92,7 @@ function LoginForm(props) {
                   name="password"
                   component={TextFieldRedux}
                   type={showPassword ? 'text' : 'password'}
-                  label="Your Password"
+                  label="Su contraseña"
                   InputProps={{
                     endAdornment: (
                       <InputAdornment position="end">
@@ -142,12 +113,12 @@ function LoginForm(props) {
               </FormControl>
             </div>
             <div className={classes.optArea}>
-              <FormControlLabel className={classes.label} control={<Field name="checkbox" component={CheckboxRedux} />} label="Remember" />
-              <Button size="small" component={LinkBtn} to="/reset-password" className={classes.buttonLink}>Forgot Password</Button>
+              <FormControlLabel className={classes.label} control={<Field name="checkbox" component={CheckboxRedux} />} label="Recordarme" />
+              <Button size="small" component={LinkBtn} to="/reset-password" className={classes.buttonLink}>¿Olvidó su contraseña?</Button>
             </div>
             <div className={classes.btnArea}>
               <Button variant="contained" color="primary" size="large" type="submit">
-                Continue
+                Continuar
                 <ArrowForward className={classNames(classes.rightIcon, classes.iconSmall)} disabled={submitting || pristine} />
               </Button>
             </div>
